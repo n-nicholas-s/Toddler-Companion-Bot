744aaa7bc84538ad3eab6c3b5c3e6f34656669af
n-nicholas-s <74168536+n-nicholas-s@users.noreply.github.com>
2020-11-09 13:19:35 +0800

    Add files via upload

---
+++ omron_sensor/sample_GUI.py
@@ -0,0 +1,57 @@
+# Sample to draw graph values acquired with D6T.
+# Please execute the following command before use "pigpio"
+# $ sudo pigpiod
+
+from __future__ import print_function
+
+import time
+import datetime
+
+import numpy as np
+import matplotlib
+matplotlib.use('TkAgg')
+from matplotlib import pyplot as plt
+
+import Omron
+
+sensor = Omron.GroveD6t()
+
+def main():
+    plt.ion()
+    fig = plt.figure()
+    x = range(5)
+    y = range(5)
+    print('start')
+    
+    while True:
+            tpn, tptat = sensor.readData()
+            if tpn != None and tptat != None:
+                print(datetime.datetime.today().strftime("[%Y/%m/%d %H:%M:%S]"),"temperature=%.1f[degC]" %tptat)
+                print(tpn[0:4])
+                print(tpn[4:8])
+                print(tpn[8:12])
+                print(tpn[12:16])
+                
+                Z = np.array([tpn[0:4],
+                          tpn[4:8],
+                          tpn[8:12],
+                          tpn[12:16]])
+
+                X,Y = np.meshgrid(x, y)
+                
+                plt.clf()
+                plt.pcolor(X,Y,Z,cmap="coolwarm",vmax=35,vmin=25)
+                
+
+                plt.xticks(x)
+                plt.yticks(y)
+                fig.tight_layout()
+                plt.colorbar()
+                fig.canvas.draw()
+                plt.show()
+                
+                
+                
+if __name__ == '__main__':
+    main()
+
